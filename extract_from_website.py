e599bc4c3cb10412b5082ca2dd4f64af5ef0ab93
Elvin Matheus <elvinmatheussousa@gmail.com>
2023-05-30 17:44:15 -0300

    Código de extração dos arquivos do portal de transparência"

---
+++ extract_from_website.py
@@ -0,0 +1,21 @@
+import requests
+import zipfile
+import io
+
+url = 'https://portaldatransparencia.gov.br/download-de-dados/viagens/2023'
+
+response = requests.get(url)
+
+if response.ok:
+
+    # Cria um objeto de fluxo de bytes a partir do conteúdo da resposta
+    zip = io.BytesIO(response.content)
+
+    # Extrai todos os arquivos do arquivo ZIP para o diretório local
+    with zipfile.ZipFile(zip, 'r') as zip_file:
+        zip_file.extractall('.')
+
+    print("Arquivo zip baixado e descompactado com sucesso.")
+
+else:
+    print("Falha ao baixar o arquivo zip.")
