39243964105ebe8cb12b2e238d770619a47d507d
Elvin Matheus <elvinmatheussousa@gmail.com>
2023-06-22 14:04:24 -0300

    Atualizações no arquivo das DAGs

--- src/viagens-dag.py
+++ src/viagens-dag.py
@@ -13,24 +13,31 @@ from copy_to_redshift import run_copy_to_redshift
 
 dag_path = os.getcwd()
 
+arquivos_esperados = ['2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv']
+
 args = {
     'owner': 'airflow',
     'start_date': datetime(2023,6,27, 3, 0),
     'schedule_interval': timedelta(minutes=10),
+    'catchup': False,
     'end_date': datetime(2023,6,27,3,10)
 }
 
-config_dag = DAG(
-    'create_bucket_S3_and_Redshift_Cluster',
-    default_args = args,
-    description = 'Cria o bucket S3 que servirá de staging e armazenará os dados processados, bem como cria um cluster gratuito no Redshift e cria uma tabela que receberá os registros para elaboração dos dashboards'
-)
+with DAG('create_bucket_S3_and_Redshift_Cluster',
+         default_args = args,
+         description = 'Cria o bucket S3 que servirá de staging e armazenará os dados processados, bem como cria um cluster gratuito no Redshift e cria uma tabela que receberá os registros para elaboração dos dashboards') as config_dag:
 
-task = BashOperator(
-    task_id = 'create_bucket_and_cluster',
-    bash_command = f'{dag_path}/create-bucket-and-cluster.sh',
-    dag = config_dag
-)
+    task1 = BashOperator(
+        task_id = 'change_permissions',
+        bash_command = 'chmod +x create-bucket-and-cluster.sh'
+    )
+
+    task2 = BashOperator(
+        task_id = 'create_bucket_and_cluster',
+        bash_command = f'{dag_path}/create-bucket-and-cluster.sh'
+    )
+
+    task1 >> task2
 
 default_args = {
     'owner': 'airflow',
@@ -40,6 +47,7 @@ default_args = {
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 3,
+    'catchup': False,
     'retry_delay': timedelta(minutes=10)
 }
 
@@ -51,64 +59,57 @@ with DAG ('viagens_a_servico_dag',
     task1 = PythonOperator(
         task_id = 'download_data_from_website',
         python_callable = run_extract_from_website,
-        dag = data_pipeline_dag
     )
 
     sensors1 = []
-    arquivos_esperados = ['2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv']
     for arquivo in arquivos_esperados:
         file_sensor = FileSensor(
             task_id = f'{arquivo}_sensor_task',
-            filepath= f'{dag_path}/{arquivo}',
+            fs_conn_id = 'fs_default',
+            filepath = arquivo,
             poke_interval=60, # Intervalo entre as verificações do arquivo em segundos
-            dag = data_pipeline_dag
-        )
+            )
         sensors1.append(file_sensor)
 
     task2 = PythonOperator(
         task_id = 'load_raw_data_to_AWS_S3',
         python_callable = run_load_raw_data,
-        dag = data_pipeline_dag
     )
 
     task3 = PythonOperator(
         task_id = 'download_data_from_AWS_S3',
         python_callable = run_extract_from_s3,
-        dag = data_pipeline_dag
     )
 
     sensors2 = []
-    arquivos_esperados = ['2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv']
     for arquivo in arquivos_esperados:
         file_sensor = FileSensor(
             task_id = f'{arquivo}_from_s3_sensor_task',
-            filepath= f'{dag_path}/raw-data/{arquivo}',
+            fs_conn_id = 'fs_default_2',
+            filepath= arquivo,
             poke_interval=60, # Intervalo entre as verificações do arquivo em segundos
-            dag = data_pipeline_dag
-        )
+            )
         sensors2.append(file_sensor)
 
     task4 = PythonOperator(
         task_id = 'transform_data',
         python_callable = run_transform,
-        dag = data_pipeline_dag
     )
 
     file_sensor = FileSensor(
         task_id = 'processed_data_sensor_task',
-        filepath = f'{dag_path}/processed-data/viagens-processed-data.csv'
+        fs_conn_id = 'fs_default_3',
+        filepath = 'viagens-processed-data.csv'
     )
 
     task5 = PythonOperator(
         task_id = 'load_processed_data_to_AWS_S3',
         python_callable = run_load_processed_data,
-        dag = data_pipeline_dag
     )
     
     task6 = PythonOperator(
         task_id = 'copy_processed_data_to_a_Redshift_cluster',
         python_callable = run_copy_to_redshift,
-        dag = data_pipeline_dag
     )
 
     # Fluxo de tarefas
