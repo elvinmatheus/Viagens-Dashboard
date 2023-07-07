1206dfc5272934909029c14318386acac6b3f26b
Elvin Matheus <elvinmatheussousa@gmail.com>
2023-07-07 09:02:09 -0300

    Melhorias adicionadas

--- src/load_raw_data_to_s3.py
+++ src/load_raw_data_to_s3.py
@@ -10,21 +10,43 @@ with open(dag_path + '/credentials.txt', 'r') as f:
     credentials = f.read()
     aws_access_key, aws_secret_key = credentials.split(',')
 
-s3_uri = f's3://viagens-a-servico-gov/{today.year}/{str(today.month).zfill(2)}/raw-data/'
+s3_uri = f's3://viagens-a-servico-gov/raw-data/'
 
 # Lista de arquivos a serem enviados para o s3
-local_files = ['2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv']
+files = ['2023_Pagamento.csv', '2023_Passagem.csv', '2023_Trecho.csv', '2023_Viagem.csv']
 
 def run_load_raw_data():
-
+    
     # Cria uma instância do sistema de arquivos S3
     fs = s3fs.S3FileSystem(key=aws_access_key, secret=aws_secret_key)
+    
+    dfs = []
 
-    # Itera sobre a lista de arquivos e faz o upload para o S3
-    for local_file in local_files:
+    try:
+    
+        for file in files:
+            df = pd.read_csv(fs.open(s3_uri + file))
+            dfs.append(df)
+    
+    except:
 
-        with fs.open(s3_uri + local_file, 'wb') as s3_file:
-            with open(f'{dag_path}/{local_file}', 'rb') as file:
-                s3_file.write(file.read())
+        # Itera sobre a lista de arquivos e faz o upload para o S3
+        for local_file in files:
 
-        os.remove(f'{dag_path}/{local_file}')
+            with fs.open(s3_uri + local_file, 'wb') as s3_file:
+                with open(f'{dag_path}/{local_file}', 'rb') as file:
+                    s3_file.write(file.read())
+
+            os.remove(f'{dag_path}/{local_file}')
+
+    else:
+
+        for i in range(4):
+            df_local = pd.read_csv(f'{dag_path}/{files[i]}')
+            df_s3 = dfs[i]
+            df_union = pd.concat([df_local, df_s3])
+            df_union = df_union.drop_duplicates()
+
+            with fs.open(s3_uri + files[i], 'wb') as s3_file:
+                with open(f'{dag_path}/{files[i]}', 'rb') as file:
+                    s3_file.write(file.read())
--- src/transform.py
+++ src/transform.py
@@ -1,9 +1,22 @@
 import pandas as pd
-import datetime as dt
 import os
 
 dag_path = os.getcwd()
-today = dt.date.today()
+
+trecho_columns = ['id_viagem', 'numero_da_proposta', 'sequencia_trecho', 'origem_data', 'origem_pais', 'origem_uf', 'origem_cidade', 'destino_data', 
+                  'destino_pais', 'destino_uf', 'destino_cidade', 'meio_de_transporte', 'numero_diarias', 'em_missao']
+
+passagem_columns = ['id_viagem', 'numero_da_proposta', 'meio_de_transporte', 'pais_origem_ida', 'uf_origem_ida', 'cidade_origem_ida', 'pais_destino_ida',
+                    'uf_destino_ida', 'cidade_destino_ida', 'pais_origem_volta', 'uf_origem_volta', 'cidade_origem_volta',
+                    'pais_destino_volta', 'uf_destino_volta', 'cidade_destino_volta', 'valor_da_passagem', 'taxa_de_servico', 'data_emissao_compra', 'hora_emissao_compra']
+
+viagem_columns = {'Identificador do processo de viagem':'id_viagem', 'Número da Proposta (PCDP)':'numero_da_proposta', 'Situação':'situacao', 'Viagem Urgente':'viagem_urgente', 
+                  'Justificativa Urgência Viagem':'justificativa_urgencia_viagem', 'Código órgão solicitante':'codigo_orgao_solicitante', 'Nome órgão solicitante':'nome_orgao_solicitante',
+                  'CPF viajante':'cpf_viajante', 'Nome':'nome', 'Cargo':'cargo', 'Função':'funcao', 'Descrição Função':'descricao_funcao', 'Período - Data de início':'periodo_data_inicio', 
+                  'Período - Data de fim':'periodo_data_fim', 'Destinos':'destinos', 'Motivo':'motivo'}
+
+pagamento_columns = ['id_viagem', 'numero_da_proposta', 'codigo_orgao_superior', 'nome_orgao_superior', 'codigo_orgao_pagador', 'nome_orgao_pagador', 'codigo_unidade_gestora_pagadora',
+                     'nome_unidade_gestora_pagadora', 'tipo_pagamento', 'valor']
 
 def run_transform():
 
@@ -15,38 +28,41 @@ def run_transform():
         if not os.path.exists(caminho + arquivo):
             raise FileNotFoundError(f'O arquivo {arquivo} não foi encontrado')
 
-    passagem = pd.read_csv(f'{dag_path}/raw-data/2023_Passagem.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem': str})
-    pagamento = pd.read_csv(f'{dag_path}/raw-data/2023_Pagamento.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem': str, 'Código do órgão superior': str, 'Codigo do órgão pagador': str, 'Código da unidade gestora pagadora': str})
-    trecho = pd.read_csv(f'{dag_path}/raw-data/2023_Trecho.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem ': str})
-    viagem = pd.read_csv(f'{dag_path}/raw-data/2023_Viagem.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem': str, 'Código órgão solicitante': str})
+    trecho = pd.read_csv(f'{dag_path}/raw-data/2023_Trecho.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem ': str}, header=0, names=trecho_columns)
+    trecho.fillna('UF/Estado/Província estrangeira', inplace=True)
+    trecho.origem_pais = trecho.origem_pais.str.title()
+    trecho.destino_pais = trecho.destino_pais.str.title()
+    trecho.origem_cidade = trecho.origem_cidade.str.title()
+    trecho.destino_cidade = trecho.destino_cidade.str.title()
 
+    passagem = pd.read_csv(f'{dag_path}/raw-data/2023_Passagem.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem ': str}, header=0, names=passagem_columns)
+    passagem.drop('meio_de_transporte', axis=1, inplace=True)
+
+    merge1 = pd.merge(trecho, passagem, left_on=['id_viagem', 'numero_da_proposta', 'origem_cidade', 'origem_uf', 'origem_pais'], 
+                                        right_on=['id_viagem', 'numero_da_proposta', 'cidade_origem_ida', 'uf_origem_ida', 'pais_origem_ida'])
+    merge1.drop(['pais_origem_ida', 'uf_origem_ida', 'cidade_origem_ida', 'pais_destino_ida', 'uf_destino_ida', 'cidade_destino_ida', 'pais_origem_volta', 
+                 'uf_origem_volta', 'cidade_origem_volta', 'pais_destino_volta', 'uf_destino_volta', 'cidade_destino_volta', 'numero_diarias'], axis=1, inplace=True)
+    merge1.fillna(method='ffill', axis=0, inplace=True)
+
+    viagem = pd.read_csv(f'{dag_path}/raw-data/2023_Viagem.csv', encoding='cp1252', sep=';', dtype={'Identificador do processo de viagem': str, 'Código órgão solicitante': str}, usecols=[0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+    viagem.rename(columns=viagem_columns, inplace=True)
+    viagem.justificativa_urgencia_viagem = viagem.justificativa_urgencia_viagem.fillna("Sem informação")
+    viagem.cpf_viajante = viagem.cpf_viajante.fillna("Informação protegida por sigilo")
+    viagem.descricao_funcao = viagem.descricao_funcao.fillna("Sem informação")
+    viagem.cargo = viagem.cargo.fillna("Sem informação")
+
+    merge2 = pd.merge(merge1, viagem, on=['id_viagem', 'numero_da_proposta'])
+    
+    pagamento = pd.read_csv(f'{dag_path}/raw-data/2023_Pagamento.csv', encoding='cp1252', sep=';', dtype={'id_viagem': str, 'codigo_orgao_superior': str, 'codigo_orgao_pagador': str, 'codigo_unidade_gestora_pagadora': str}, header=0, names=pagamento_columns)
     pagamento.rename(columns={'Codigo do órgão pagador':'Código do órgão pagador'}, inplace=True)
-    trecho.rename(columns={'Identificador do processo de viagem ':'Identificador do processo de viagem'}, inplace=True)
-    trecho = trecho.drop('Meio de transporte', axis=1)
+    pagamento = pagamento[pagamento.tipo_pagamento == 'PASSAGEM'] 
+    pagamento.drop(['tipo_pagamento', 'valor'], axis=1, inplace=True)
+    pagamento.drop_duplicates(subset=['id_viagem', 'numero_da_proposta'], inplace=True, keep='first')
+    pagamento.nome_orgao_superior = pagamento.nome_orgao_superior.str.title()
+    pagamento.nome_orgao_pagador = pagamento.nome_orgao_pagador.str.title()
+    pagamento.nome_orgao_pagador = pagamento.nome_orgao_pagador.fillna("Sem Informação")
 
-    merge1 = pd.merge(trecho, passagem, left_on=['Identificador do processo de viagem', 'Número da Proposta (PCDP)', 'Origem - Cidade', 'Origem - UF', 'Origem - País'], right_on=['Identificador do processo de viagem', 'Número da Proposta (PCDP)', 'Cidade - Origem ida', 'UF - Origem ida', 'País - Origem ida' ])
-
-    merge1 = merge1.drop(['País - Origem ida', 'UF - Origem ida',
-                        'Cidade - Origem ida', 'País - Destino ida', 'UF - Destino ida',
-                        'Cidade - Destino ida', 'País - Origem volta', 'UF - Origem volta',
-                        'Cidade - Origem volta', 'Pais - Destino volta', 'UF - Destino volta',
-                        'Cidade - Destino volta', 'Número Diárias'], axis=1)
-
-    viagem = viagem.drop(['Código do órgão superior', 'Nome do órgão superior', 'Valor diárias', 'Valor passagens', 'Valor devolução', 'Valor outros gastos'], axis=1)
-    viagem.drop_duplicates(inplace=True)
-
-    merge2 = pd.merge(merge1, viagem, on=['Identificador do processo de viagem', 'Número da Proposta (PCDP)'])
-
-    pagamento = pagamento[pagamento['Tipo de pagamento'] == 'PASSAGEM']
-    pagamento = pagamento.drop(['Tipo de pagamento', 'Valor'], axis=1)
-    pagamento.drop_duplicates(subset=['Identificador do processo de viagem', 'Número da Proposta (PCDP)'], inplace=True, keep='first')
-
-    merge3 = pd.merge(merge2, pagamento, on=['Identificador do processo de viagem', 'Número da Proposta (PCDP)'])
-
-    merge3['Valor da passagem'] = merge3['Valor da passagem'].str.replace(',', '.')
-    merge3['Valor da passagem'] = pd.to_numeric(merge3['Valor da passagem'])
-    merge3['Taxa de serviço'] = merge3['Taxa de serviço'].str.replace(',', '.')
-    merge3['Taxa de serviço'] = pd.to_numeric(merge3['Taxa de serviço'])
+    merge3 = pd.merge(merge2, pagamento, on=['id_viagem', 'numero_da_proposta'])
 
     processed_data_dir = f'{dag_path}/processed-data'
     if not os.path.exists(processed_data_dir):
